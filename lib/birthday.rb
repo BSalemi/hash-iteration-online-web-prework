8d5161f3a049597ecd3a7fab5afadf7681f5d58c
Brian Salemi <briandsalemi@gmail.com>
2019-05-01 16:40:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age =< 9
       puts "Happy Birthday #{name}! You are now #{age} years old!"
-    else 
+    else
       puts "You are too old for this."
     end
   end
